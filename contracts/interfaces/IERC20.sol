199f9eef400b0c09f7261ca150fbaed58b3bf533
Jacob Evans <dekz@dekz.net>
2019-12-06 02:20:42 -0800

    Revert "WIP Frontend"

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.9;
 pragma experimental ABIEncoderV2;
 
+
 interface IERC20 {
+
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function transfer(address, uint256) external returns (bool);
 
 }
--- contracts/interfaces/IStructs.sol
+++ contracts/interfaces/IStructs.sol
@@ -10,13 +10,13 @@ interface IStructs {
         address yAddress;                                   // address of token y
         int256 x;                                           // contract's balance of token x (fixed point)
         int256 y;                                           // contract's balance of token y (fixed point)
-        uint256 l;                                          // total liquidity token balance
         int256 pBarX;                                       // expected future price of x in terms of y (fixed point)
         uint256 rhoNumerator;
         int256 rhoRatio;
         int256 fee;
         uint256 t;                                          // most recent block
         mapping (address => uint256) liquidityBalance;
+        uint256 l;                                          // total liquidity token balance
         int256 beta;    // persistence of expercted price - the larger the more persistent
         int256 eToKappa;   // clamp that prevents the expected price changing by a lot in an expected tx
     }
