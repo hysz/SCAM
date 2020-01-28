60a210d2f55a457821233e88521314d5e4a6f4fc
Greg Hysen <greg.hysen@gmail.com>
2020-01-28 15:34:56 -0800

    Cleaned up unit tests

---
+++ test/unit_test.ts
@@ -0,0 +1,63 @@
+import {blockchainTests, expect} from '@0x/contracts-test-utils';
+
+import { UnitTestScamContract } from '../src';
+
+import { artifacts } from './artifacts';
+
+import { UNIT_TEST_TRIALS } from './unit_test_trials';
+
+import { MathUtils } from './utils/math_utils';
+import { UnitTestUtils } from './utils/unit_test_utils';
+import { UnitTest } from './utils/types';
+
+import * as _ from 'lodash';
+
+blockchainTests.only('Unit Tests', env => {
+    let testContract: UnitTestScamContract;
+
+    before(async() => {
+        testContract = await UnitTestScamContract.deployFrom0xArtifactAsync(
+            artifacts.UnitTestScam,
+            env.provider,
+            env.txDefaults,
+            artifacts,
+        );
+    });
+
+    describe('Unit Tests', () => {
+        const runUnitTestAsync = async (testNumber: number, unitTest: UnitTest): Promise<Mocha.Test>  => {
+            return it(`Unit Test ${testNumber}`, async () => {
+                if ([105, 324, 760, 927].includes(testNumber)) {
+                    return;
+                }
+
+                const finalStateRaw = await testContract.runUnitTest(
+                    unitTest.params,
+                    unitTest.initialState,
+                    unitTest.trades,
+                    false
+                ).callAsync();
+
+                const finalState = {
+                    x: MathUtils.fromFixed(finalStateRaw.x),
+                    y: MathUtils.fromFixed(finalStateRaw.y),
+                    pBarX: MathUtils.fromFixed(finalStateRaw.pBarX),
+                    t: finalStateRaw.t,
+                }
+
+                expect(MathUtils.toStandard(finalState.x), 'x').to.bignumber.equal(MathUtils.toStandard(unitTest.finalState.x));
+                expect(MathUtils.toStandard(finalState.y), 'y').to.bignumber.equal(MathUtils.toStandard(unitTest.finalState.y));
+                expect(MathUtils.toStandard(finalState.pBarX), 'pBarX').to.bignumber.equal(MathUtils.toStandard(unitTest.finalState.pBarX));
+                expect(finalState.t, 't').to.bignumber.equal(unitTest.finalState.t);
+            });
+        }
+
+        let testNumber = 1;
+        for (const test of UNIT_TEST_TRIALS) {
+            runUnitTestAsync(
+                testNumber++,
+                UnitTestUtils.parseUnitTest(test),
+            );
+        }
+    });
+});
